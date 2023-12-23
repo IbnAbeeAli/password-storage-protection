c407d1fba8de1763b2a68b2f86f8168e5c717cc1
Muhammad Danish <mdanishkhdev@gmail.com>
2023-12-24 01:02:48 +0500

    Algo skeleton for hashing (#2)
    * Algo skeleton for hashing
    
    * conflict

---
+++ algo.py
@@ -0,0 +1,36 @@
+import hashlib
+import timeit
+
+def hash_password(salt, password, work_factor, pepper):
+    # Create a string that concatenates the salt, the password, the work factor, and the pepper
+    string = salt + password + pepper
+
+    # Hash the string 2^work_factor times
+    for _ in range(2 ** int(work_factor)):
+        string = hashlib.sha256(string.encode()).hexdigest()
+
+    # Return the hashed string
+    return string
+
+
+# Create a function that times how long it takes to hash a password, and then returns an optimal work factor
+# The work factor should be the smallest integer that results in a hash time of at least target_time_in_seconds
+
+def find_work_factor(salt, password, pepper, target_time_in_seconds=1):
+    # Initialize the work factor
+    work_factor = 0
+
+    # Initialize the hash time
+    hash_time = 0
+
+    # Loop until the hash time is at least 0.1 seconds
+    while hash_time < target_time_in_seconds:
+
+        # Increment the work factor
+        work_factor += 1
+
+        # Calculate the hash time
+        hash_time = timeit.timeit(stmt=lambda: hash_password(salt, password, work_factor, pepper), number=1)
+
+    # Return the work factor
+    return work_factor
--- db.py
+++ db.py
@@ -1,22 +1,47 @@
 import sqlite3
+from algo import find_work_factor, hash_password
 
-# Connect to the SQLite database
-conn = sqlite3.connect('test.db')
+def main():
+    # Connect to the SQLite database
+    conn = sqlite3.connect('test.db')
 
-# Create a cursor object to execute SQL queries
-cursor = conn.cursor()
+    # Create a cursor object to execute SQL queries
+    cursor = conn.cursor()
 
-# Execute a SELECT query to retrieve all rows from a specific table
-table_name = 'user_credentials'  # Replace 'your_table' with the actual name of your table
-cursor.execute(f"SELECT * FROM {table_name}")
+    # Execute a SELECT query to retrieve all rows from a specific table
+    table_name = 'user_credentials'
+    cursor.execute(f"SELECT * FROM {table_name}")
 
-# Fetch all rows from the result set
-rows = cursor.fetchall()
+    # Fetch all rows from the result set
+    rows = cursor.fetchall()
 
-# Print the rows
-for row in rows:
-    print(row)
+    # Print the rows
+    for row in rows:
+        print(row)
 
-# Close the cursor and the connection
-cursor.close()
-conn.close()
+    # Close the cursor and the connection
+    cursor.close()
+    conn.close()
+
+    # Create a salt
+    salt = 'salt'
+
+    # Create a password
+    password = 'password'
+
+    # Create a pepper
+    pepper = 'pepper'
+
+    # Find the work factor
+    work_factor = find_work_factor(salt, password, pepper)
+
+    # Hash the password
+    hashed_password = hash_password(salt, password, work_factor, pepper)
+
+    # Print the work factor and the hashed password
+    print(f'Work factor: {work_factor}')
+    print(f'Hashed password: {hashed_password}')
+
+
+if __name__ == '__main__':
+    main()
