9cd5543105501e0d7a3aab73fc6f2530eb535e73
Muhammad Danish <mdanishkhdev@gmail.com>
2023-12-24 01:00:48 +0500

    conflict

--- db.py
+++ db.py
@@ -1,36 +1,3 @@
-<<<<<<< Updated upstream
-import mysql.connector
-
-# Replace these with your actual MySQL database credentials
-db_config = {
-    'host': '127.0.0.1',
-    'user': 'root',
-    'password': 'seecs@123',
-    'database': 'topsecret',
-}
-
-# Establish a connection to the MySQL server
-connection = mysql.connector.connect(**db_config)
-
-# Create a cursor object to interact with the database
-cursor = connection.cursor()
-
-
-# Show the contents of the table
-show_table_query = "SELECT * FROM user_credentials"
-cursor.execute(show_table_query)
-
-# Fetch all the rows
-rows = cursor.fetchall()
-
-# Display the results
-for row in rows:
-    print(row)
-
-# Close the cursor and connection
-cursor.close()
-connection.close()
-=======
 import sqlite3
 from algo import find_work_factor, hash_password
 
@@ -78,4 +45,3 @@ def main():
 
 if __name__ == '__main__':
     main()
->>>>>>> Stashed changes
